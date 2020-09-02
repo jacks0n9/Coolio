40b54458268f10f1d72d37b2a1fadb93cb3d6f4b
elnepik <67214151+elnepik@users.noreply.github.com>
2020-09-01 20:29:11 -0700

    Update coolioupdate.sh

--- coolioupdate.sh
+++ coolioupdate.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 rm /usr/local/bin/coolio
-curl -o /usr/local/bin https://raw.githubusercontent.com/elnepik/Coolio/master/coolio
+curl -o /usr/local/bin/coolio https://raw.githubusercontent.com/elnepik/Coolio/master/coolio
+chmod +x /usr/local/bin/coolio
 rm /usr/local/bin/coolioupdate.sh
 cd
