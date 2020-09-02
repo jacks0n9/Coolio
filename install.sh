c83dfb776caac1c65b7a5b259313c5bc160cf2f4
elnepik <67214151+elnepik@users.noreply.github.com>
2020-09-02 06:35:10 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-curl -o /usr/local/bin/coolio https://raw.githubusercontent.com/elnepik/Coolio/master/coolio
+curl -S -s -o /usr/local/bin/coolio https://raw.githubusercontent.com/elnepik/Coolio/master/coolio
 chmod +x /usr/local/bin/coolio
 
