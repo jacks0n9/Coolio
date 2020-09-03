dac367373241b080296185450465a0ec545673fb
elnepik <67214151+elnepik@users.noreply.github.com>
2020-09-02 19:42:08 -0700

    Changed cd placement

--- coolioupdate.sh
+++ coolioupdate.sh
@@ -3,5 +3,6 @@ rm /usr/local/bin/coolio
 curl -S -s -o /usr/local/bin/coolio https://raw.githubusercontent.com/elnepik/Coolio/master/coolio
 chmod +x /usr/local/bin/coolio
 curl -Ss https://raw.githubusercontent.com/elnepik/Coolio/master/install.sh | bash
-rm /usr/local/bin/coolioupdate.sh
 cd
+rm /usr/local/bin/coolioupdate.sh
+
