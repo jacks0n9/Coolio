75252aed1ca77fd6c8e4d42a77959867d0631bb8
elnepik <67214151+elnepik@users.noreply.github.com>
2020-09-02 18:59:57 -0700

    Bruh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 curl -S -s -o /usr/local/bin/coolio https://raw.githubusercontent.com/elnepik/Coolio/master/coolio
-curl -Ss -o /usr/local/bin/createcool.sh 
+curl -Ss -o /usr/local/bin/createcool.sh https://raw.githubusercontent.com/elnepik/Coolio/master/createcool.sh
+chmod +x /usr/local/bin/createcool.sh
 chmod +x /usr/local/bin/coolio
 
