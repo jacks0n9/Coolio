32838de0400fee768812389eb6568525cc8646d0
elnepik <67214151+elnepik@users.noreply.github.com>
2020-09-02 19:40:01 -0700

    Figured out how to update properly

--- coolioupdate.sh
+++ coolioupdate.sh
@@ -2,6 +2,6 @@
 rm /usr/local/bin/coolio
 curl -S -s -o /usr/local/bin/coolio https://raw.githubusercontent.com/elnepik/Coolio/master/coolio
 chmod +x /usr/local/bin/coolio
-rm /usr/local/bin/coolioupdate.sh
 curl -Ss https://raw.githubusercontent.com/elnepik/Coolio/master/install.sh | bash
+rm /usr/local/bin/coolioupdate.sh
 cd
