a45464aa205aa8cb81506634e793a81fc274dbc3
elnepik <67214151+elnepik@users.noreply.github.com>
2020-09-02 20:15:09 -0700

    Update coolioupdate.sh

--- coolioupdate.sh
+++ coolioupdate.sh
@@ -4,7 +4,7 @@ curl -S -s -o /usr/local/bin/coolio https://raw.githubusercontent.com/elnepik/Co
 chmod +x /usr/local/bin/coolio
 curl -Ss https://raw.githubusercontent.com/elnepik/Coolio/master/install.sh | bash
 rm createcool.sh
-curl -Sso /usr/local/bin/createcool.sh https://raw.githubusercontent.com/elnepik/Coolio/master/createcool.sh
+curl -Ss -o /usr/local/bin/createcool.sh https://raw.githubusercontent.com/elnepik/Coolio/master/createcool.sh
 chmod +x /usr/local/bin/createcool.sh
 cd
 rm /usr/local/bin/coolioupdate.sh
